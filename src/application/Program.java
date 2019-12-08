1fe76c10c57ed2f96e8272ac636e2cd72b0e1911
Lucas Seguessi <lucas.seguessi@gmail.com>
2019-12-08 13:00:56 -0300

    Override

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,30 +10,42 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Account acc = new Account(1001, "Alex", 0.0);
-		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+			/*Account acc = new Account(1001, "Alex", 0.0);
+			BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+			
+			//Upcasting
+			Account acc1 = bacc;
+			Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+			Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+			
+			//Downcasting
+			BusinessAccount acc4 = (BusinessAccount)acc2;
+			acc4.loan(100.0);
+			
+			//BusinessAccount acc5 = (BusinessAccount)acc3;
+			if(acc3 instanceof BusinessAccount) {
+				BusinessAccount acc5 = (BusinessAccount)acc3;
+				acc5.loan(200.0);
+				System.out.println("loan");
+			}
+			
+			if(acc3 instanceof SavingsAccount) {
+				SavingsAccount acc5 = (SavingsAccount)acc3;
+				acc5.updateBalance();
+				System.out.println("Update");
+			}*/
 		
-		//Upcasting
-		Account acc1 = bacc;
-		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		Account acc1 = new Account(1001, "Alex", 1000.0);
+		acc1.withdraw(200.0);
+		System.out.println(acc1.getBalance());
 		
-		//Downcasting
-		BusinessAccount acc4 = (BusinessAccount)acc2;
-		acc4.loan(100.0);
+		Account acc2 = new SavingsAccount(1002, "Anna", 1000.0, 0.1);
+		acc2.withdraw(200.0);
+		System.out.println(acc2.getBalance());
 		
-		//BusinessAccount acc5 = (BusinessAccount)acc3;
-		if(acc3 instanceof BusinessAccount) {
-			BusinessAccount acc5 = (BusinessAccount)acc3;
-			acc5.loan(200.0);
-			System.out.println("loan");
-		}
-		
-		if(acc3 instanceof SavingsAccount) {
-			SavingsAccount acc5 = (SavingsAccount)acc3;
-			acc5.updateBalance();
-			System.out.println("Update");
-		}
+		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+		acc3.withdraw(200.0);
+		System.out.println(acc3.getBalance());
 		
 	}
 
