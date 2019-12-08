c5d12a85b1f56e14dab0b2a8bba8943f9283d333
Lucas Seguessi <lucas.seguessi@gmail.com>
2019-12-08 12:42:45 -0300

    Exemplo herança

---
+++ src/entities/Account.java
@@ -0,0 +1,45 @@
+package entities;
+
+public class Account {
+
+	private Integer number;
+	private String holdeder;
+	protected Double balance;
+	
+	public Account() {}
+
+	public Account(Integer number, String holdeder, Double balance) {
+		this.number = number;
+		this.holdeder = holdeder;
+		this.balance = balance;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public String getHoldeder() {
+		return holdeder;
+	}
+
+	public void setHoldeder(String holdeder) {
+		this.holdeder = holdeder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+	
+	public void deposit(double amount) {
+		balance += amount;
+	}
+	
+}
